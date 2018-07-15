b05bc1d32e4ee6637c0dd895b2b227e7454f6661
surajsinghnegi <surajsinghnegi009@gmail.com>
2018-07-15 14:28:30 +0530

    first commit

---
+++ platforms/android/app/src/main/assets/www/js/script.js
@@ -0,0 +1,39 @@
+function adSetter(){
+// alert(navigator.userAgent);
+var admobid = {};
+// select the right Ad Id according to platform
+if( /(android)/i.test(navigator.userAgent) ) { 
+    admobid = { // for Android
+        banner: 'ca-app-pub-7532993595031090/8624123929',
+        interstitial: 'ca-app-pub-7532993595031090/3180225552'
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: 'ca-app-pub-7532993595031090/8624123929',
+        interstitial: 'ca-app-pub-7532993595031090/3180225552'
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: 'ca-app-pub-7532993595031090/8624123929',
+        interstitial: 'ca-app-pub-7532993595031090/3180225552'
+    };
+}
+
+if(AdMob) AdMob.createBanner( {
+    isTesting:true, //Remove this Before publishing your app
+    adId:admobid.banner, 
+    position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+    autoShow:true} );
+      if(window.AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+
+
+}
+  function onDeviceReady(){
+  // alert("device ready");
+      adSetter();
+  }
+
+
+window.onload=function(){
+ document.addEventListener("deviceready", onDeviceReady, false);
+}
